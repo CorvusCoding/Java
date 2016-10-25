7ca15a8e373c95867013c33b08f6125861074e40
CorvusCoding <sidneybartlett13@gmail.com>
2016-10-25 12:34:40 -0700

    Delete test.java

--- test.java
+++
@@ -1,8 +0,0 @@
-public class Test{
-public static void main(String args[]){
-	int i = 2 , j = 4;
-	int allOnes = ~0
-	int left = allOnes << (j+1);
-	Systems.out.println("Binary Equivalent at this stage: " +Interger.toBinaryString(left));
-	}
-}
