6b3348a70e22676a170090a9630670f9b2b7a94f
CorvusCoding <sidneybartlett13@gmail.com>
2016-10-25 12:33:48 -0700

    Delete Hello2.java

--- Hello2.java
+++
@@ -1,8 +0,0 @@
-//save this file in a folder called java you have created in home
-//hello.java
-
-class Hello2 {
-	public static void main (String a[]){
-		System.out.print("Hello 2 world!");
-	}
-}
