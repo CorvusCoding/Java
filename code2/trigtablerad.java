2c5642e45b7b9855df3971ec71b0c8678e102a58
CorvusCoding <sidneybartlett13@gmail.com>
2017-05-19 12:06:09 -0700

    Add files via upload

---
+++ code2/trigtablerad.java
@@ -0,0 +1,20 @@
+class trigtablerad {
+	public static void main (String args[]) {
+		double c,s,t;
+		int d = 0; 
+		double r = 0, rround = 0;
+		double pi = 3.141592;
+		double i = (pi/12);
+		String undefined = "UN";
+		System.out.println ("radian measurement is represented as rad");
+		System.out.println("\t\t rad \t\t| cos(radian) \t\t| sin(rad \t\t| tan (rad)" );
+		while (r <= (2*pi)){
+			c = Math.cos(r);
+			s = Math.sin(r);
+			t = Math.tan(r);
+			rround = (Math.round(r)*1000);
+			System.out.println("\t\t" + r + "\t\t | " + c +"\t\t"+ s + "\t\t" + t);
+			r = r + i;
+		}
+	}
+}
